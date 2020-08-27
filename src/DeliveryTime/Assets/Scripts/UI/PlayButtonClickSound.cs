47674429da981bda4c0bd54d80cbf80a729d72af
Silas Reinagel <silas.reinagel@carvana.com>
2020-08-27 13:10:50 -0700

    Evolved Icons. Closes #22. Closes #23. Closes #18

--- src/DeliveryTime/Assets/Scripts/UI/PlayButtonClickSound.cs
+++ src/DeliveryTime/Assets/Scripts/UI/PlayButtonClickSound.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public sealed class PlayButtonClickSound : MonoBehaviour
 {
     [SerializeField] private AudioClip sound;
